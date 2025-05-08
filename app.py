67d37530f0d296fbe77fe43e29a8393bda99129c
athamaulana096 <athamaulana30@gmail.com>
2025-05-08 15:41:59 +0700

    changing port to default

--- app.py
+++ app.py
@@ -415,4 +415,6 @@ def get_segment_image():
     return jsonify({'image': img_base64})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
